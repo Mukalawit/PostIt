2ee755bfdf7fdb9061e2bc1d048d148f1b521cc7
mukalawit <mgstn15@gmail.com>
2022-11-30 17:04:45 +0300

    test: unique username , email and integral signup

--- server/__tests__/signup.tests.js
+++ server/__tests__/signup.tests.js
@@ -1,20 +1,20 @@
 const request = require('supertest');
-const { PrismaClient } = require('@prisma/client');
+const { User } = require('../models');
+const { prismaMock } = require('../db/singleton');
 
-const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: 'postgresql://postgres:postgres@localhost:5432/tests?schema=public',
-    },
-  },
-});
+const record = {
+  username: 'username',
+  password: 'password',
+  email: 'test@test.com'
+};
+
+const { username, email, password } = record;
+
+const user = new User(username, email, password);
 
 const baseURL = 'http://localhost:3000';
 
 describe('When a user signs up', () => {
-  beforeEach(async () => {
-    await prisma.user.deleteMany({});
-  });
 
   it('should fail if no username is submitted', async () => {
     const response = await request(baseURL).post('/api/user/signup').send({
@@ -42,28 +42,27 @@ describe('When a user signs up', () => {
     expect(response.body.message).toBe('Invalid Payload');
   });
 
-  it('should add a new user', async () => {
-    const response = await request(baseURL).post('/api/user/signup').send({
-      username: 'username',
-      email: 'testing@test.com',
-      password: 'password',
-    });
-    expect(response.statusCode).toBe(201);
+
+  it('throws an error if email is already in use', async () => {
+    prismaMock.user.findUnique.mockResolvedValue({ email });
+
+    await expect(User.isUnique({ email })).rejects.toThrow('email is already in use');
+
   });
 
-  it('should not add duplicate users', async () => {
-    const response1 = await request(baseURL).post('/api/user/signup').send({
-      username: 'username',
-      email: 'testing@test.com',
-      password: 'password',
-    });
+  it('throws an error if username is already in use', async () => {
+    prismaMock.user.findUnique.mockResolvedValue({ username });
 
-    const response2 = await request(baseURL).post('/api/user/signup').send({
-      username: 'username',
-      email: 'testing@test.com',
-      password: 'password',
-    });
+    await expect(User.isUnique({ username })).rejects.toThrow('username is already in use');
+  });
 
-    expect(response2.statusCode).toBe(409);
+  it('should return user on signup', async () => {
+    prismaMock.user.create.mockResolvedValue(record);
+
+    await expect(user.save()).resolves.toEqual({
+      username: 'username',
+      password: 'password',
+      email: 'test@test.com'
+    });
   });
 });
