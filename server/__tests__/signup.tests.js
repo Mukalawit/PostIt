506e1d4e6ff05499313b99bfa16af251bdcf50c2
mukalawit <mgstn15@gmail.com>
2022-12-07 15:36:35 +0300

    test: sign up routes

--- server/__tests__/signup.tests.js
+++ server/__tests__/signup.tests.js
@@ -1,24 +1,34 @@
 const request = require('supertest');
 const { User } = require('../models');
-const { prismaMock } = require('../db/singleton');
+const app = require('../server');
+const HttpError = require('../utils');
+
+const baseURL = 'http://localhost:3001';
+jest.mock('../models', () => ({
+  User: jest.fn()
+}));
+let server;
+
+beforeAll(() => {
+  server = app.listen(3001);
+});
+
+afterAll(() => {
+  server.close();
+});
 
 const record = {
   username: 'username',
-  password: 'password',
-  email: 'test@test.com'
+  email: 'testing@test.com',
+  password: 'password'
 };
 
 const { username, email, password } = record;
-
-const user = new User(username, email, password);
-
-const baseURL = 'http://localhost:3000';
-
 describe('When a user signs up', () => {
   it('should fail if no username is submitted', async () => {
     const response = await request(baseURL).post('/api/user/signup').send({
-      email: 'testing@test.com',
-      password: 'password',
+      email,
+      password,
     });
     expect(response.statusCode).toBe(400);
     expect(response.body.message).toBe('Invalid Payload');
@@ -26,40 +36,41 @@ describe('When a user signs up', () => {
 
   it('should fail if no email is submitted', async () => {
     const response = await request(baseURL).post('/api/user/signup').send({
-      username: 'username',
-      password: 'password',
+      username,
+      password,
     });
     expect(response.statusCode).toBe(400);
     expect(response.body.message).toBe('Invalid Payload');
   });
   it('should fail if no username is submitted', async () => {
     const response = await request(baseURL).post('/api/user/signup').send({
-      email: 'testing@test.com',
-      password: 'password',
+      email,
+      password,
     });
     expect(response.statusCode).toBe(400);
     expect(response.body.message).toBe('Invalid Payload');
   });
-
-  it('throws an error if email is already in use', async () => {
-    prismaMock.user.findUnique.mockResolvedValue({ email });
-
-    await expect(User.isUnique({ email })).rejects.toThrow('email is already in use');
-  });
-
-  it('throws an error if username is already in use', async () => {
-    prismaMock.user.findUnique.mockResolvedValue({ username });
-
-    await expect(User.isUnique({ username })).rejects.toThrow('username is already in use');
-  });
-
-  it('should return user on signup', async () => {
-    prismaMock.user.create.mockResolvedValue(record);
-
-    await expect(user.save()).resolves.toEqual({
-      username: 'username',
-      password: 'password',
-      email: 'test@test.com'
+  it('should add a new user', async () => {
+    User.mockImplementationOnce(() => {
+      jest.fn(() => Promise.resolve());
     });
+    const response = await request(baseURL).post('/api/user/signup').send({
+      username,
+      email,
+      password,
+    });
+    expect(response.statusCode).toBe(201);
+  });
+
+  it('should not add duplicate users', async () => { 
+    User.mockImplementationOnce(() => {
+      jest.fn(() => Promise.reject(new HttpError(409, 'user already exists')));
+    });
+    const response = await request(baseURL).post('/api/user/signup').send({
+      username,
+      email,
+      password,
+    });
+    expect(response.statusCode).toBe(409);
   });
 });
