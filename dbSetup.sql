2849c4372e811b39f34ca75a77f318ba54248cdd
mukalawit <mgstn15@gmail.com>
2022-12-16 14:29:59 +0300

    chore: add database setup script

---
+++ dbSetup.sql
@@ -0,0 +1,7 @@
+/*Run this command in POSTICO
+
+If you do not have postico please download it at https://eggerapps.at/postico2/download 
+
+Postico is only available for mac users */
+
+CREATE DATABASE PostIt;
