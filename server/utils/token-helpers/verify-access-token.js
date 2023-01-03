0a495109b85658e091f4ffb75d407103ec7f9027
mukalawit <mgstn15@gmail.com>
2023-01-03 22:20:42 +0300

    chore: remove unused code

--- server/utils/token-helpers/verify-access-token.js
+++ server/utils/token-helpers/verify-access-token.js
@@ -4,7 +4,6 @@ const jwt = require('jsonwebtoken');
 function verifyAccessToken(token) {
   try {
     const record = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-    console.log(record)
     return record;
   } catch (error) { return false; }
 }
