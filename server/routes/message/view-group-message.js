0d5dc68383b9b7fb2b51c030b6429849131ff8a8
mukalawit <mgstn15@gmail.com>
2023-01-12 15:50:21 +0300

    feat: view group messages

---
+++ server/routes/message/view-group-message.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { HttpError } = require('../../utils/errors');
+const { Message } = require('../../models');
+const { authenticateToken } = require('../../utils/token-helpers');
+
+const router = express.Router();
+router.get('/:id/messages', authenticateToken, async (req, res) => {
+    const { id } = req.user;
+    const groupId  = Number(req.params.id);
+
+    try {
+        const message = new Message(id,groupId);
+        await message.view();
+        return res.status(200).json({ message: 'Viewing Messages' });
+    } catch (error) {
+        if (error instanceof HttpError) {
+            return res.status(error.statusCode).json({ message: error.message });
+        }
+        return res.status(500).json({ message: error.message });
+    }
+});
+module.exports = router;
